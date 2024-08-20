16b0075a8a0d1c09c774e1299480fa69bb753a71
yousse7f <104494233+yousse7f@users.noreply.github.com>
2024-08-20 08:18:43 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,6 +3,7 @@ function toggleMenu() {
     sidenav.style.display = (sidenav.style.display == 'none' || sidenav.style.display == '') ? 'block' : 'none';
 }
 
+// تحوي لالارقام الهندية الى العربية في حال تم ادخال الارقام الهندية
 document.addEventListener("DOMContentLoaded", () => {
     const hindiToArabicMap = {
         '٠': '0',
@@ -25,20 +26,41 @@ document.addEventListener("DOMContentLoaded", () => {
     window.convertHindiToArabic = convertHindiToArabic;
 });
 
-
 function setupCellValidation(inputCell, statusCell, correctValue) {
-    inputCell.addEventListener("input", () => {
-        const value = convertHindiToArabic(inputCell.value.trim());
-        const isCorrect = value === correctValue;
-        const feedback = isCorrect ? "✔" : `✘\n${value}\nالجواب: ${correctValue}`;
-        statusCell.textContent = feedback;
-        statusCell.style.color = isCorrect ? "green" : "red";
-    });
+    const value = convertHindiToArabic(inputCell.value.trim());
+    const isCorrect = value === correctValue;
+    const feedback = isCorrect ? "✔" : `✘\n${value}\nالجواب: ${correctValue}`;
+    statusCell.textContent = feedback;
+    statusCell.style.color = isCorrect ? "green" : "red";
+    return isCorrect;
 }
 
 
-const inputs = document.querySelectorAll("input[type='text']");
-const spans = document.querySelectorAll("span");
+
+document.getElementById("showResultsBtn").addEventListener("click", () => {
+    const loader = document.getElementById("loader");
+    loader.style.display = "block";
+
+    setTimeout(() => {
+        loader.style.display = "none";
+        const inputs = document.querySelectorAll("input[type='text']");
+        const spans = document.querySelectorAll("span");
 
 
-
+// التحقق من الاجابة 
+          let correctCount = 0;
+                for (let i = 0; i < inheritanceRows.length; i++) {
+                    const input = inputs[i];
+                    const span = spans[i];
+                    const isCorrect = setupCellValidation(input, span, inheritanceRows[i]);
+                    if (isCorrect) {
+                        correctCount++;
+                    }
+                }
+// تحويل الدرجات الى النسبة المئوية
+        const totalQuestions = inheritanceRows.length;
+        const percentage = (correctCount / totalQuestions) * 100;
+        const percentageDisplay = document.getElementById("percentageDisplay");
+        percentageDisplay.textContent = `النسبة المئوية: ${percentage.toFixed(2)}%`;
+    }, 3000); // الانتظار لمدة ثلاث ثواني قبل إظهار النتائج
+});
